2a3d48c9af0a1361f818f4826ecd77051476fc9e
sigma2647 <1169446204@qq.com>
2025-04-21 22:55:39 +0800

    upda

--- tmux/.config/tmux/current_path.sh
+++ tmux/.config/tmux/current_path.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-awk -F '/' '{if(NF > 4){print ""$(NF-1)"/"$(NF)}else{print}}' </dev/stdin
+# awk -F '/' '{if(NF > 4){print ""$(NF-1)"/"$(NF)}else{print}}' </dev/stdin
+
+awk -F '/' '{if(NF > 4){printf "%s/%s\n", $ (NF-1), $NF} else {print}}' </dev/stdin
