ff05213e8a6db6605dc46c5bb13dbb84d63e16b3
sigma647 <1169446204@qq.com>
2025-06-19 23:52:52 +0800

    add fish add glow as my markdown preview in yazi cli

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,41 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+# if status is-interactive
+#     # Commands to run in interactive sessions can go here
+# end
+
+
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+
+alias g='lazygit'
+alias nvimrc='nvim ~/.config/nvim/'
+alias v='nvim'
+alias t='tmux'
+alias l='y'
+
+alias ta='tmux attach'
+
+alias oo="cd $HOME/note/sigma"
+alias dot='cd ~/dotfile'
+alias pc='proxychains4'
+
 
 starship init fish | source
+
+set -Ux EDITOR nvim
+set -Ux VISUAL nvim
+
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
