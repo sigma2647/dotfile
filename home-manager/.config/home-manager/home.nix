28f80d3458243ad5a6f0811f9497eec50dff5289
sigma2647 <1169446204@qq.com>
2025-07-20 00:07:40 +0800

    aadup

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -13,7 +13,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -49,7 +49,8 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
-    ".config/starship".sourece = /home/lawrence/dotfile/starship/.config;
+
+    # ".config/starship".sourece = /home/lawrence/dotfile/starship/.config;
   };
 
   # Home Manager can also manage your environment variables through
